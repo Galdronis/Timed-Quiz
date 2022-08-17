f332c0b918bf749bc4b4ef8c6f48717ab4245f69
Nicholas148894 <nich.oettinger@gmail.com>
2022-08-17 09:51:00 -0400

    cleaned up code and preparing to add functionality

---
+++ assets/js/game.js
@@ -0,0 +1,48 @@
+var choices = Array.from(document.querySelectorAll('.choiceLetter'))
+var question = document.querySelector('.question')
+var timerCount = document.querySelector('#timerCount')
+var score = document.querySelector('#score')
+
+var questionsArray = [
+    {
+        question: 'What is a Rallentando?', 
+        A: 'When you suddenly get loud.',
+        B: 'When you gradually speed up.',
+        C: 'When you gradually slow down.',
+        D: 'When you suddenly get quiet.',
+        answer: C,
+    },
+    
+]
+
+
+
+Question one: What is a Rallentando? 
+A. When you suddenly get loud.
+B. When you gradually speed up.
+C. When you gradually slow down.
+D. When you suddenly get quiet. 
+
+Question two: What does the top number in a time signature mean?
+A. It indicates the number of beats in a measure.
+B. It indicates the number of measures in the length the piece.
+C. It indicates the length value of any given note.
+D. It indicates the number of times a given section is intended to be played.
+
+Question three: What does "Dulce" mean in music?
+A. The music is intended to be played fiercly.
+B. The music is intended to be played sweetly.
+C. The music is intended to be played mournfully.
+D. The music is made of chocolate.
+
+Question four: What is a "Nocturne?"
+A. Music inspired by whimsy.
+B. Music inspired by the night.
+C. Music inspired by graveyards.
+D. Music inspied by the stars.
+
+Question five: What is objectively the best type of musical instrument?
+A. Percussion
+B. Percussion
+C. Percussion
+D. Percussion-->
